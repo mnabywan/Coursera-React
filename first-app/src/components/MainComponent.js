0f39f7245dd834bed750928779595753e362b52c
Mateusz Nabywaniec <mateusz.nabywaniec@gmail.com>
2021-10-02 17:12:45 +0200

    Single page application part 1

--- first-app/src/components/MainComponent.js
+++ first-app/src/components/MainComponent.js
@@ -1,12 +1,15 @@
 import Menu from './MenuComponent'
 import React, { Component } from 'react';
-import { DISHES } from '../shared/dishes'
 import DishDetail from './DishdetailComponent'
 import Home from './HomeComponent';
 import Header from './HeaderComponent'
 import Footer from './FooterCompoonent';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
-
+import { DISHES } from '../shared/dishes'
+import { COMMENTS } from '../shared/comments'
+import { LEADERS } from '../shared/leaders'
+import { PROMOTIONS } from '../shared/promotions'
 
 class Main extends Component {
   constructor(props){
@@ -14,6 +17,9 @@ class Main extends Component {
 
     this.state = {
       dishes : DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
       // selectedDish : null
     }
   };
@@ -26,7 +32,9 @@ class Main extends Component {
   render() {
     const HomePage = () => {
       return(
-        <Home/>
+        <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]} 
+          promotion={this.state.promotions.filter((promotion) => promotion.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}/>
       )
     }
 
@@ -37,6 +45,7 @@ class Main extends Component {
           <Route path="/home" component={HomePage}/> // does not allow to pass ant props
           <Route exact path="/menu" 
             component={() => <Menu dishes={this.state.dishes}/>}/> //better way we can pass props
+          <Route exact path="contactus" component={Contact}/>
           <Redirect to="/home" />
 
         </Switch>
